c347c67263043f503ef85d221c3e1f6cc8409801
Jankees <mail@jankeesvw.com>
2018-12-16 12:03:03 +0100

    Rotate document and make it a JPEG

--- index.js
+++ index.js
@@ -6,36 +6,37 @@ import { createCanvas } from "canvas";
 import fetchGasUsage from "./fetchGasUsage";
 import fetchTemperatures from "./fetchTemperatures";
 import "isomorphic-fetch";
+import im from "imagemagick";
 
 dotenv.config();
 
 const gasUsage = fetchGasUsage();
 const temperatures = fetchTemperatures();
 
-const canvas = createCanvas(384, 640);
+const canvas = createCanvas(640, 384);
 const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "white";
-ctx.fillRect(0, 0, 384, 640);
+ctx.fillRect(0, 0, 640, 384);
 
 ctx.fillStyle = "black";
 
 _.forEach(gasUsage.values, (data, i) => {
   const height = data[1] * 2;
-  ctx.fillRect(i * 45 + 30, 50, 10, -height);
+  ctx.fillRect(i * 45 + 30, 90, 30, -height);
 });
 
-ctx.font = '19px Courier';
+ctx.font = "15px Courier";
 
 _.forEach(temperatures, (thermostat, i) => {
   ctx.fillText(thermostat.name, 10, i * 20 + 150);
-  ctx.fillText(thermostat.setpoint, 110, i * 20 + 150);
-  ctx.fillText(thermostat.temperature, 150, i * 20 + 150);
+  ctx.fillText(thermostat.setpoint, 130, i * 20 + 150);
+  ctx.fillText(thermostat.temperature, 200, i * 20 + 150);
 });
 
-const out = createWriteStream("dashboard.png"),
-  stream = canvas.createPNGStream();
-
-stream.on("data", function(chunk) {
-  out.write(chunk);
+const out = createWriteStream(__dirname + "/dashboard.jpeg");
+const stream = canvas.createJPEGStream();
+stream.pipe(out);
+out.on("finish", () => {
+  return console.log("The JPEG file was created.");
 });
